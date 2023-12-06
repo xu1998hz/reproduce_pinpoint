cc22fd2675520434d0621f52dc6d6334ef58cc31
Guanglei Zhu <gzhu@br014.ib.bridges2.psc.edu>
2023-12-05 20:18:41 -0500

    add test ft code

--- qa_run.sh
+++ qa_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -p GPU-shared
-#SBATCH -t 4:30:00
-#SBATCH --gpus=v100-32:1
+#SBATCH -t 10:00:00
+#SBATCH --gpus=v100-32:2
 #SBATCH --output=/ocean/projects/cis230075p/gzhu/reproduce_pinpoint/slurm_out/qa.out
 
-./main_run.sh -i asqa_test.tsv -b 1 -m 7 -p fp16 -o out/ -t qa -d False
+./main_run.sh -i asqa_test.tsv -b 1 -m 13 -p fp16 -o out/ -t qa -d False
