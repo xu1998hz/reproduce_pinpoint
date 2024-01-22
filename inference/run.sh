0c07400d53f18867c05bee1f5b3329639647fac6
IanGuangleiZhu <guanglei.zhu@mail.utoronto.ca>
2024-01-22 18:42:19 +0000

    minor

--- inference/run.sh
+++ inference/run.sh
@@ -3,12 +3,12 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=64GB
-#SBATCH --gpus=4
-#SBATCH --time=16:00:00
+#SBATCH --gpus=1
+#SBATCH --time=24:00:00
 #SBATCH --account=guangleizhu
 #SBATCH --partition=taurus
-#SBATCH --output=/home/guangleizhu/reproduce_pinpoint/slurm_out/ft_llama.out
-#SBATCH --error=/home/guangleizhu/reproduce_pinpoint/slurm_out/ft_llama_error.out
+#SBATCH --output=/home/guangleizhu/reproduce_pinpoint/slurm_out/mt_eval_test.out
+#SBATCH --error=/home/guangleizhu/reproduce_pinpoint/slurm_out/mt_eval_test_error.out
 
 source ~/.bashrc
 # module purge
@@ -25,7 +25,7 @@ nvidia-smi
 # - username should be replaced by your username
 # - path-to-directory should be replaced by the path to your directory where the executable is
 
-cd /home/guangleizhu/reproduce_pinpoint/finetune
+cd /home/guangleizhu/reproduce_pinpoint/inference
 
 # deepspeed --include localhost:0,1,4,5,7 finetune_llama.py
-deepspeed --num_gpus 4 finetune_llama.py
+python3 test.py
