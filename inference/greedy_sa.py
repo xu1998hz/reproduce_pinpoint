5dcddd9ad3a34ceb34674dfd5eb5ac902af0564f
xu1998hz <wedxu@ucdavis.edu>
2024-01-24 12:38:50 +0000

    update all bugs in greedy sa

--- inference/greedy_sa.py
+++ inference/greedy_sa.py
@@ -29,12 +29,15 @@ def parse_feedback(text):
     l = text.split('\n')
     out = ''
     for line in l[1:]:
-        match = re.search(r'Error Location \d: \'(.*)\', Error Type: (.*), Severity: (Major|Minor)', line)
-        location = match.group(1)
-        error_type = match.group(2).lower()
-        severity = match.group(3).lower()
-        s = f"'{location}' is a {severity} {error_type} error."
-        out += s
+        try:
+            match = re.search(r'Error Location \d: \'(.*)\', Error Type: (.*), Severity: (Major|Minor)', line)
+            location = match.group(1)
+            error_type = match.group(2).lower()
+            severity = match.group(3).lower()
+            s = f"'{location}' is a {severity} {error_type} error."
+            out += s
+        except:
+            continue
     return out
 
 # add language
@@ -80,41 +83,42 @@ def base_generate(src, lang, model, tokenizer, config, device, candidate=None, f
             instances = [{
                 'src': '如果逾期，逾期记录将记录到个人信用报告中，可能会对日后买车、购房等经济生活造成不良影响。',
                 'mt': 'If overdue, overdue records will be recorded in your personal credit report, which may negatively impact future car purchases, real estate transactions, and other economic living situations.',
-                'feedback': "Your translation contains 1 errors.\nError Location 1: 'overdue', Error Type: Accuracy/Mistranslation, Severity: Major",
-                'revised': 'If overdue, overdue records will be recorded in your personal credit report, which may negatively impact future car purchases, real estate transactions, and other economic living situations.'
+                'feedback': "'overdue' is a major accuracy/mistranslation.",
+                'revised': 'If it is overdue, overdue records will be recorded in your personal credit report, which may negatively impact future car purchases, real estate transactions, and other economic living situations.'
             }]
     else:
         instances = []
     instances.append({'src': src, 'mt': candidate, 'feedback': feedback, 'revised': None})
 
     for i in instances:
-        src_prompt = f"Translate the following {src_lang} source into {target_lang} translation. Give only one clean {target_lang} translation without any explanation. {src_lang} source:\n{i['src']}\n{target_lang} translation:"
+        src_prompt = f"Translate the following {src_lang} source into {target_lang} translation. Give only one clean {target_lang} translation without any explanation. {src_lang} source: {i['src']} {target_lang} translation:"
         messages.append({"role": "user", "content": src_prompt})
         if i['feedback']:
-            feedback_prompt = f"{feedback} Please revise the {target_lang} translation according to my feedback. Provide a clean {target_lang} translation without any explanation. {src_lang} source:\n{src}\n{target_lang} translation:"
+            feedback_prompt = f"{i['feedback']} Please revise the {target_lang} translation according to my feedback. Provide a clean {target_lang} translation without any explanation. {target_lang} translation:"
             messages.extend([
-                {"role": "assistant", "content": candidate},
+                {"role": "assistant", "content": i['mt']},
                 {"role": "user", "content": feedback_prompt},
             ])
         # only True for icl examples
         if i['revised']:
-            messages.append({"role": "assistant", "content": candidate})
+            messages.append({"role": "assistant", "content": i['revised']})
     prompt = tokenizer.apply_chat_template(messages, tokenize=False)
     batch = tokenizer(prompt, return_tensors='pt', padding=False).to(device)
+    print(prompt)
     input_ids = batch['input_ids']
     attention_mask = batch['attention_mask']
     model.eval()
-
     outputs = model.generate(
         input_ids, 
         attention_mask=attention_mask, 
         max_new_tokens=512, 
-        pad_token_id=tokenizer.eos_token_id
+        pad_token_id=tokenizer.eos_token_id,
+        temperature=0
     )
     out = tokenizer.decode(outputs[0], skip_special_tokens=True)
     input = tokenizer.decode(input_ids[0], skip_special_tokens=True)
-    out = out.replace(input, '')
-    out = out[1:]
+    out = out.replace(input, '').strip()
+
     if feedback:
         print(src, '|', candidate, '|', feedback)
         print('after correction'.center(60, '-'))
@@ -150,7 +154,14 @@ def feedback(args):
         else:
             f_lst.append(f)
     print(f'number of no feedback: {c} ({c / len(data["src"])})')
-    return f_lst    
+    return f_lst   
+
+
+"""Load feedback from a local file and use those feedback for correction step"""
+def load_feedback(feedback_addr):
+    feedback_ls = ''.join(open(feedback_addr, 'r').readlines()).split('[SPECIAL_TOK_WENDA]')
+    feedback_ls = [parse_feedback(f) for f in feedback_ls]
+    return feedback_ls
 
 def correction(feedback, args):
     assert args.lang in ['en-de', 'zh-en']
@@ -159,10 +170,12 @@ def correction(feedback, args):
     # load mistral 7b instruct model
     if args.model == 'mistral':
         base_name = 'mistralai/Mistral-7B-Instruct-v0.2'
-        base_tokenizer = AutoTokenizer.from_pretrained(base_name, use_fast=True)
-        base_model = AutoModelForCausalLM.from_pretrained(base_name, device_map=device, torch_dtype=torch.float32)
     else:
-        raise NotImplementedError
+        base_name = 'NousResearch/Llama-2-7b-chat-hf'
+    print(f"loading {base_name}")
+
+    base_tokenizer = AutoTokenizer.from_pretrained(base_name, use_fast=True)
+    base_model = AutoModelForCausalLM.from_pretrained(base_name, torch_dtype=torch.float32).to(device)
 
     with open(args.data_path, 'r') as f:
         data = json.load(f)
@@ -187,15 +200,22 @@ def correction(feedback, args):
 if __name__ == "__main__":
     argparse = argparse.ArgumentParser()
     argparse.add_argument('--wmt', default='wmt22')
-    argparse.add_argument('--lang', default='en-de')
+    argparse.add_argument('--lang', default='zh-en')
     argparse.add_argument('--model', default='mistral')
-    argparse.add_argument('--model_addr', default='/ocean/projects/cis230075p/gzhu/reproduce_pinpoint/finetune/ft_out/en-de/checkpoint-770')
-    argparse.add_argument('--data_path', default='/ocean/projects/cis230075p/gzhu/reproduce_pinpoint/out/mt_out/comet_scores_en-de_wmt_test_wmt22_mistral.json')
-    argparse.add_argument('--out_path', default='/ocean/projects/cis230075p/gzhu/reproduce_pinpoint/out/test.json')
+    argparse.add_argument('--model_addr', default='/mnt/taurus/home/guangleizhu/reproduce_pinpoint/finetune/ft_out/zh-en/checkpoint-760/')
+    argparse.add_argument('--data_path', default='out/mt_out/comet_scores_zh-en_wmt_test_wmt22_mistral.json')
+    argparse.add_argument('--out_path', default='out/mt_out/correction_zh-en_wmt_test_wmt22_mistral.json')
     # FIXME: batch not working rn
     argparse.add_argument('--batch_size', default=1)
     argparse.add_argument('--max_length', default=720)
+    argparse.add_argument('--feedback_addr', default='out/comet_scores_zh-en_wmt_test_wmt22_mistral.txt')
+    argparse.add_argument('--feedback_type', help='improve, score, binary, mqm')
     args = argparse.parse_args()
     print(args)
-    f = feedback(args)
-    correction(f, args)
+
+    if args.feedback_addr:
+        f_ls = load_feedback(args.feedback_addr)
+    else:
+        f_ls = feedback(args)
+
+    correction(f_ls, args)
